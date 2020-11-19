1c77b8f27332f3e1d4edf453bae912761a79dd26
Ashish Chandwani <ashishchandwani295@gmail.com>
2020-11-20 05:28:00 +0530

    11/20-Token Generation from Spotify and extraction

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import React, { useEffect } from 'react';
 import './App.css';
 import Login from './Login';
+import { getAccessTokenFromResponse } from './spotify';
 
 function App() {
+
+  useEffect(() => {
+      const hash = getAccessTokenFromResponse();
+      window.location.hash = "";
+      const token = hash.access_token;
+  }, [])
+
   return (
     <div className="app">
       <Login />
